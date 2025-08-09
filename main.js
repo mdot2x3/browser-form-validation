f0f15ff23558d354809c28384f1c99eb50925482
mdot2x3 <mdot2x3@gmail.com>
2025-08-08 23:11:54 -0700

    feat: build form, style, add email validation
    create validation form structure in index.html.
    
    add basic styles in style.css.
    
    add email validation logic in main.js to check for
    valid email formatting that displays a custom
    error message and reruns per user key stroke and
    on form submission.

---
+++ main.js
@@ -0,0 +1,71 @@
+const form = document.querySelector("form");
+const email = document.querySelector("#email");
+const country = document.querySelector("#country");
+const postal = document.querySelector("#postal");
+const password = document.querySelector("#password");
+const confirm = document.querySelector("#confirm");
+const submit = document.querySelector("#submit");
+
+const emailError = document.querySelector("#email-error");
+const countryError = document.querySelector("#country-error");
+const postalError = document.querySelector("#postal-error");
+const passwordError = document.querySelector("#password-error");
+const confirmError = document.querySelector("#confirm-error");
+const error = document.querySelector(".error-message");
+
+// regular expression for email validation as per HTML specification
+const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;
+
+// check if the email is valid
+const isValidEmail = () => {
+  const validity = email.value.length !== 0 && emailRegExp.test(email.value);
+  return validity;
+};
+
+// update email input class based on validity
+const setEmailClass = (isValid) => {
+  email.className = isValid ? "valid" : "invalid";
+};
+
+// update error message and visibility
+const updateError = (isValidInput) => {
+  if (isValidInput) {
+    emailError.textContent = "";
+    emailError.removeAttribute("class");
+  } else {
+    emailError.textContent = "Please enter a valid email.";
+    emailError.setAttribute("class", "active");
+  }
+};
+
+// initialize email validity on page load
+const initializeValidation = () => {
+  const emailInput = isValidEmail();
+  setEmailClass(emailInput);
+};
+
+// handle input event to update email validity
+const handleInput = () => {
+  const emailInput = isValidEmail();
+  setEmailClass(emailInput);
+  updateError(emailInput);
+};
+
+// handle form submission to show error if email is invalid
+const handleSubmit = (event) => {
+  // prevent default submit to server action
+  event.preventDefault();
+
+  const emailInput = isValidEmail();
+  setEmailClass(emailInput);
+  updateError(emailInput);
+};
+
+// now we can rebuild our validation constraint
+// because we do not rely on css pseudo-class, we have to
+// explicitly set the valid/invalid class on our email field
+window.addEventListener("load", initializeValidation);
+// this defines what happens when the user types in the field
+email.addEventListener("input", handleInput);
+// this defines what happens when the user tries to submit the data
+form.addEventListener("submit", handleSubmit);
